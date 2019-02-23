a4a7dec7416215980e90cba4ffad9ad5fcd4d1f8
algebraic-propagation-5468 <luishong50@gmail.com>
2019-02-23 03:05:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,11 +30,10 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
- function funkyFunction(){
- var theFunk = funkyFunction() {
-   return funkyFunction() {
-     return "FUNKY!"
-   }
- }
- }
+ 
+ var theFunk = funkyFunction 
+   
+ 
+ 
+   
 
